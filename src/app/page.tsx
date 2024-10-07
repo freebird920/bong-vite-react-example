0cf32f6ad8567606ee83a264244910d12f754787
퇴역군인 <freebird_han@naver.com>
2024-10-07 16:20:57 +0900

    홈 페이지에 GitHub 리포지토리 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ const HomePage = memo(() => {
     <>
       <h1>봉우리코더</h1>
 
+      <h2>Git Hub Repository</h2>
+      <a href="https://github.com/freebird920/bong-react-vite-jangin">
+        봉우리코더 프로젝트 리포지토리
+      </a>
+
       <h2>예제</h2>
       <a href="/241010">241010</a>
     </>
