93b28ce404e65c6647dc5faccb7c5fe5743fed3e
freebird920 <freebird_han@naver.com>
2024-10-14 20:00:36 +0900

    ㅕㅔ

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import App241009 from "./app/App241009/page.tsx";
 import AppFinal from "./app/AppFinal/page.tsx";
 import App241017 from "./app/App241017/page.tsx";
 import Homework241010 from "./app/Homework241010/page.tsx";
+import App241024 from "./app/App241024/page.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
   {
     path: "/241017",
     element: <App241017 />,
+  },
+  {
+    path: "/241024",
+    element: <App241024 />,
   },
   {
     path: "/hw241010",
