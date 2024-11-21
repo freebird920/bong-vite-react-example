75fdb0c4cf0e04d6ab6837ae8b7ca48ba3bbdf8a
퇴역군인 <freebird_han@naver.com>
2024-11-21 14:58:11 +0900

    update

--- src/app/App241121/page.tsx
+++ src/app/App241121/page.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo } from "react";
-
+import "./page.css";
 const App = () => {
   const [position, setPosition] = useState([0, 1]); // [row, col]
   const myMap = useMemo(() => {
